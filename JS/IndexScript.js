7742738915e1fb633cb396a1f0966799dfaa11f0
MaisonBertrand <93873899+MaisonBertrand@users.noreply.github.com>
2023-11-04 17:09:33 -0500

    Dynamically Create Links

---
+++ JS/IndexScript.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fileListContainer = document.getElementById('file-list');
+
+    // List of file names (you can fetch this dynamically or from a server)
+    const fileNames = ['file1.html', 'file2.html', 'file3.html'];
+
+    fileNames.forEach(function (fileName) {
+        const link = document.createElement('a');
+        link.href = `html_files/${fileName}`; // Set the path to your HTML files
+
+        const cube = document.createElement('div');
+        cube.classList.add('cube');
+        cube.textContent = fileName.replace('.html', '');
+
+        link.appendChild(cube);
+        fileListContainer.appendChild(link);
+    });
+});
